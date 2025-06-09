2c335a40a39b91ef4bdc26d68fab5e8b1f468718
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-06-09 15:14:20 +0300

    Reintroduce user dashboard, payment, and home pages with updated layout component

--- app/[lang]/(user)/layout.tsx
+++ app/[lang]/(user)/layout.tsx
@@ -1,7 +1,7 @@
-"use client";
+// "use client";
 import React from "react";
 import { auth } from "@/lib/auth";
-const session = await auth();
+// const session = await auth();
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
