e93a42095b7b93537c1864e8701c45edfb93da9d
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-06-11 08:03:37 +0300

    feat: Add deposit and withdrawal schemas for wallet operations

--- actions/user/wallet.tsx
+++ actions/user/wallet.tsx
@@ -1,3 +1,10 @@
+"use server";
+import prisma from "@/lib/db";
+import { signupSchema } from "@/lib/zodSchema";
+import { z } from "zod";
+// import { v4 as uuidv4 } from "uuid";
+import { auth } from "@/lib/auth";
+
 export async function deposit() {}
 
 export async function DepositHistory() {}
--- lib/zodSchema.ts
+++ lib/zodSchema.ts
@@ -42,3 +42,32 @@ export const createGroupSchema = z.object({
   isPublic: z.boolean(),
 });
 export type CreateGroupType = z.infer<typeof createGroupSchema>;
+
+export const depositSchema = z.object({
+  amount: z.number().min(0, "Amount must be positive"),
+  photo: z.any().optional(), // Accept any file type, validation can be handled elsewhere
+});
+export type DepositType = z.infer<typeof depositSchema>;
+
+export const withdrawSchema = z.object({
+  amount: z.number().min(0, "Amount must be positive"),
+  transactionPassword: z
+    .string()
+    .min(8, "Transaction password must be at least 8 characters long"),
+});
+export type WithdrawType = z.infer<typeof withdrawSchema>;
+
+export const updateProfileSchema = z.object({
+  name: z.string().min(2, "Name is too short"),
+  email: z.string().email("Invalid email address"),
+  phone: z.string().min(9, "Phone number is too short"),
+  // photo: z.any().optional(), // Accept any file type, validation can be handled elsewhere
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .optional(),
+  confirmPassword: z
+    .string()
+    .min(8, "Confirm password must be at least 8 characters long")
+    .optional(),
+});
